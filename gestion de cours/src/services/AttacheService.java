80682b2d127ef64a0a3452c9c0d3cced6558ef52
Papa Mbaye <167906580+Mnaye-2024@users.noreply.github.com>
2024-05-16 20:50:21 +0000

    Premier commit

--- gestion de cours/src/services/AttacheService.java
+++ gestion de cours/src/services/AttacheService.java
@@ -10,22 +10,18 @@ public class AttacheService {
     private List<Etudiant> etudiants = new ArrayList<>();
     private List<SessionCours> sessionsCours = new ArrayList<>();
 
-    // Méthode pour inscrire un étudiant
     public boolean inscrireEtudiant(Etudiant etudiant) {
         return etudiants.add(etudiant);
     }
 
-    // Méthode pour planifier une session de cours
     public boolean planifierSessionCours(SessionCours sessionCours) {
         return sessionsCours.add(sessionCours);
     }
 
-    // Méthode pour afficher tous les étudiants
     public List<Etudiant> listerEtudiants() {
         return new ArrayList<>(etudiants);
     }
 
-    // Méthode pour afficher toutes les sessions de cours
     public List<SessionCours> listerSessionsCours() {
         return new ArrayList<>(sessionsCours);
     }
