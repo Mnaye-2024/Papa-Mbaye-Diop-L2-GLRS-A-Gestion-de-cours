496802ca215d84442ac71637c7d6843ead01d080
Papa Mbaye <167906580+Mnaye-2024@users.noreply.github.com>
2024-05-16 20:49:54 +0000

    Premier commit

--- gestion de cours/src/services/AdminService.java
+++ gestion de cours/src/services/AdminService.java
@@ -10,22 +10,18 @@ public class AdminService {
     private List<Professeur> professeurs = new ArrayList<>();
     private List<Cours> cours = new ArrayList<>();
 
-    // Méthode pour ajouter un professeur
     public boolean ajouterProfesseur(Professeur professeur) {
         return professeurs.add(professeur);
     }
 
-    // Méthode pour créer un cours
     public boolean creerCours(Cours cours) {
         return this.cours.add(cours);
     }
 
-    // Méthode pour afficher tous les professeurs
     public List<Professeur> listerProfesseurs() {
         return new ArrayList<>(professeurs);
     }
 
-    // Méthode pour afficher tous les cours
     public List<Cours> listerCours() {
         return new ArrayList<>(cours);
     }
